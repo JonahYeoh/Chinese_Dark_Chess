2377aa0781c43f09f981ec8d6869c744b85c67b9
Jonah <47838117+JonahYeoh@users.noreply.github.com>
2020-06-13 22:18:04 +0800

    Create STATE.h

---
+++ STATE.h
@@ -0,0 +1,42 @@
+#include "STATE.h"
+State::State()
+{
+	memset( board, 0, 32 );
+}
+
+State::State(const State & s )
+{
+	setBoard( s.board );
+	color = s.color;
+}
+
+State::~State( void ) {  }
+
+void State::setBoard( const int src[32] )
+{
+	for ( int i = 0; i < 32; i++ ) board[i] = src[i];	
+}
+
+void State::switchColor()
+{
+	color = ( color == 0 ) ? 1 : 0;
+}
+
+int State::getColor(){	return color; }
+
+void State::setColor( const int c ) { color = c; }
+
+void State::walkover( int mov[2] )
+{
+	char piece = board[mov[0]];
+	board[mov[0]] = '-';
+	board[mov[1]] = piece;
+}
+
+void State::getBoard( int dst[32] )
+{
+	for ( int i = 0; i < 32; i++ )
+		dst[i] = board[i];
+}
+
+
